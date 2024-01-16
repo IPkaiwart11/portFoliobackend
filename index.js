40e3e9f65f2fddda6ee5fc98437157651fb75763
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-16 23:25:59 +0530

    initial 2

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
 import userroute from './routes/User.js'
 import cors from 'cors';
-
 const app = express();
 
-const connect = async()=>{
-            try{
-               await mongoose.connect(process.env.mongoDB);
-               console.log('connected to mongodb');
-            } catch(error){
-               console.log("error is beta",error);
-            }
-            
-}
+
+const connect = async () => {
+   try {
+     await mongoose.connect(process.env.mongoDB);
+     console.log('Connected to MongoDB');
+   } catch (error) {
+     console.error('Error connecting to MongoDB:', error.message);
+   }
+ };
+ console.log('MongoDB Connection String:', process.env.mongoDB);
 
 mongoose.connection.on('disconnection',()=>{
                console.log('mongodb disconnected')
