921ab25e33ef1ba6270fc1cac3be6cdb47b9d39d
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-20 00:54:48 +0530

    added modul

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ const Userschema =new mongoose.Schema({
         },
         country:{
                type: String,
+               required:true,
         },
         message:{
               type:String,
